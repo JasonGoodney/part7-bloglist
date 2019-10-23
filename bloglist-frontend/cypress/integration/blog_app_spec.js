b976a72a8e55d0d09aa11abfbb3743800ac41ffc
Jason Goodney <jason.goodney@gmail.com>
2019-10-23 15:59:26 -0400

    adds tests for liking and commenting on a blog

--- bloglist-frontend/cypress/integration/blog_app_spec.js
+++ bloglist-frontend/cypress/integration/blog_app_spec.js
@@ -1,4 +1,15 @@
 describe('Blog', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Elyse Willems',
+      username: 'elysew',
+      password: 'thebige'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
+    cy.visit('http://localhost:3000')
+  })
+
   it('front page can be opened', function() {
     cy.visit('http://localhost:3000')
     cy.contains('Blogs')
@@ -14,6 +25,58 @@ describe('Blog', function() {
     cy.get('.usernameInput').type('elysew')
     cy.get('.passwordInput').type('thebige')
     cy.contains('submit').click()
-    cy.contains('elyse willems')
+    cy.contains('Elyse Willems')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('.usernameInput').type('elysew')
+      cy.get('.passwordInput').type('thebige')
+      cy.contains('submit').click()
+    })
+
+    it('name of the user is shown', function() {
+      cy.contains('Elyse Willems')
+    })
+
+    it('a new blog can be created', function() {
+      cy.contains('create new').click()
+      cy.get('.titleInput').type('Cypress Blog')
+      cy.get('.authorInput').type('Cypress Testing')
+      cy.get('.urlInput').type('cypress.io')
+      cy.contains('save').click()
+      cy.contains('Cypress Blog')
+    })
+
+    describe.only('and a blog is created', function() {
+      beforeEach(function() {
+        cy.contains('create new').click()
+        cy.get('.titleInput').type('Another Cypress Blog')
+        cy.get('.authorInput').type('Cypress Tester')
+        cy.get('.urlInput').type('cypress.io')
+        cy.contains('save').click()
+        cy.contains('Another Cypress Blog')
+      })
+
+      it('can be selected', function() {
+        cy.contains('Another Cypress Blog').click()
+      })
+
+      it('user can like blog', function() {
+        // cy.wait(6000) /// Wait for notification to dismiss
+        cy.get(':nth-child(3) > div > a').click()
+        cy.get('.likeButton').click()
+        cy.contains("you liked 'Another Cypress Blog'")
+      })
+
+      it('user can add comment', function() {
+        // cy.wait(6000) /// Wait for notification to dismiss
+        cy.get(':nth-child(3) > div > a').click()
+        cy.get('#commentInput').type('comment by cypress')
+        cy.contains('Add comment').click()
+        cy.contains('comment by cypress')
+      })
+    })
   })
 })
